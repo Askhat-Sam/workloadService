60a9f0def2561e37e2ae864d871d5cade9efd3bd
Askhat <Askhat.Sam@gmail.com>
2025-05-26 11:51:19 +0500

    init: initial commit

--- src/main/java/dev/workload/microservice/service/interfaces/WorkloadService.java
+++
@@ -1,8 +0,0 @@
-package dev.workload.microservice.service.interfaces;
-
-import dev.workload.microservice.entity.TrainingWorkload;
-import dev.workload.microservice.entity.TrainingWorkloadRequest;
-
-public interface WorkloadService {
-    TrainingWorkload addTrainingWorkload(TrainingWorkloadRequest trainingWorkload);
-}
